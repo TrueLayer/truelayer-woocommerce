957aa7e139c71e92aa3dadd6797f08471679d787
Milena Bimbasic <milena.bimbasic@maksimer.com>
2023-03-30 14:47:16 +0200

    Format code

--- classes/class-truelayer-gateway.php
+++ classes/class-truelayer-gateway.php
@@ -160,8 +160,8 @@ class TrueLayer_Payment_Gateway extends WC_Payment_Gateway {
 	 * @return array
 	 */
 	public function process_payment( $order_id ) {
-               $settings = get_option( 'woocommerce_truelayer_settings', array() );
-               $epp_enabled  = $settings['truelayer_payment_page_type'] ?? 'HPP';
+                $settings = get_option( 'woocommerce_truelayer_settings', array() );
+                $epp_enabled  = $settings['truelayer_payment_page_type'] ?? 'HPP';
 
 		$response = TrueLayer()->api->create_payment( $order_id );
 
@@ -182,15 +182,15 @@ class TrueLayer_Payment_Gateway extends WC_Payment_Gateway {
 
 		$build_test_url = Truelayer_Helper_Hosted_Payment_Page_URL::build_hosted_payment_page_url( $order_id );
 
-               if( 'EPP' === $epp_enabled ) {
-                       $encoded_url = base64_encode( rawurlencode( home_url( '/wc-api/TrueLayer_Redirect/' ) ) );
-                       $encoded_payment_id = base64_encode( $truelayer_payment_id );
-                       $encoded_payment_token = base64_encode( $truelayer_payment_token );
-                       return array(
-                               'result'   => 'success',
-                               'redirect' => '#truelayer:' . $encoded_url . ':' . $encoded_payment_id . ':' . $encoded_payment_token, // phpcs:ignore
-                       );
-               }
+                if( 'EPP' === $epp_enabled ) {
+                        $encoded_url = base64_encode( rawurlencode( home_url( '/wc-api/TrueLayer_Redirect/' ) ) );
+                        $encoded_payment_id = base64_encode( $truelayer_payment_id );
+                        $encoded_payment_token = base64_encode( $truelayer_payment_token );
+                        return array(
+                                'result'   => 'success',
+                                'redirect' => '#truelayer:' . $encoded_url . ':' . $encoded_payment_id . ':' . $encoded_payment_token, // phpcs:ignore
+                        );
+                }
 
 		return array(
 			'result'   => 'success',
